8b96ed12e59be6f172f66ecad7789845bc76f5e5
ksupdev <karoons.updev@gmail.com>
2025-04-12 23:56:46 +0700

    Create condition for change style

--- src/components/layer/Province.js
+++ src/components/layer/Province.js
@@ -6,7 +6,40 @@ import data from '../data/province.json'
 
 const Province = () => {
 
-    return data && <GeoJSON data={data} />;
+    const geoStyle = (feature) => {
+        const region = feature.properties.REGION6;
+        console.log(region);
+        if (region == 'ภาคกลาง') {
+            return {
+                weight: 1,
+                color: 'red',
+                fillColor: 'red',
+                fillOpacity: 0.3
+            }
+        } else if (region == 'ภาคตะวันออกเฉียงเหนือ') {
+            return {
+                weight: 1,
+                color: 'green',
+                fillColor: 'green',
+                fillOpacity: 0.3
+            }
+        } else if (region == 'ภาคเหนือ') {
+            return {
+                weight: 1,
+                color: 'yellow',
+                fillColor: 'yellow',
+                fillOpacity: 0.3
+            }
+        } else{
+            return {
+                weight: 1
+            }
+        }
+    }
+
+    return data && <GeoJSON data={data}
+        style={geoStyle}
+    />;
 }
 
 export default Province
